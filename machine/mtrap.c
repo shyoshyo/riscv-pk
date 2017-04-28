817d3df7a89f91a26e4cd7d3751a69684e24d97d
ZhangWei <suvtab@gmail.com>
2017-04-28 21:43:26 +0800

    Get BBL ready for RV32

--- machine/mtrap.c
+++ machine/mtrap.c
@@ -263,6 +263,9 @@ void mcall_trap(uintptr_t* regs, uintptr_t mcause, uintptr_t mepc)
       retval = mcall_remote_fence_i((uintptr_t*)arg0);
       break;
     default:
+      // Workaround for creating first user process in Lab 5
+      // Can we do it better?
+      redirect_trap(mepc, read_csr(mstatus));
       retval = -ENOSYS;
       break;
   }
